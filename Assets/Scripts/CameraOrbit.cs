ead5c52a334bc4e9f2b6b46af0a48b0424ad68bf
JUAN-SABATER\juan-sabater <juan.sabater@socialpoint.es>
2019-09-04 18:31:27 +0200

    updates camera

--- Assets/Scripts/CameraOrbit.cs
+++ Assets/Scripts/CameraOrbit.cs
@@ -12,18 +12,38 @@ public class CameraOrbit : MonoBehaviour
     public float maxFOV = 100f;
     public float sensitivity = 17f;
 
+    bool movingWithoutCube = false;
+
     void Update()
     {
-        if (Input.GetMouseButton(1))
+        if (Input.GetMouseButton(0))
         {
-            transform.RotateAround(target.transform.position, transform.up, Input.GetAxis("Mouse X") * speed);
-            transform.RotateAround(target.transform.position, transform.right, Input.GetAxis("Mouse Y") * -speed);
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            //            var raycast 
+            RaycastHit hit;
+            var foundCube = false;
+            if(Physics.Raycast(ray, out hit))
+            {
+                foundCube = string.Equals(hit.collider.gameObject.tag, "Cube");
+                movingWithoutCube = movingWithoutCube ? true : !foundCube;
+            }
+
+            if(movingWithoutCube)
+            {
+                transform.RotateAround(target.transform.position, transform.up, Input.GetAxis("Mouse X") * speed);
+                transform.RotateAround(target.transform.position, transform.right, Input.GetAxis("Mouse Y") * -speed);
+            }
         }
 
         float fov = Camera.main.fieldOfView;
         fov += Input.GetAxis("Mouse ScrollWheel") * -sensitivity;
         fov = Mathf.Clamp(fov, minFOV, maxFOV);
         Camera.main.fieldOfView =  fov;
+
+        if(Input.GetMouseButtonUp(0))
+        {
+            movingWithoutCube = false;
+        }
 
     }
 }
